9091be63823792ffda764ab42a85fed2efd1b16f
Ysmael Marinho Santos Júnior <ysmael.santos@meta.com.br>
2020-10-08 20:28:16 -0300

    jwt e documentacao

---
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,56 @@
+package br.com.alura.forum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	//injecao do valor do application.properties
+	@Value("${forum.jwt.expiration}")
+	private String expiration;
+	
+
+	@Value("${forum.jwt.secret}")
+	private String secret;
+
+	public String gerarToken(Authentication authentication) {
+		
+		Usuario usuarioLogado = (Usuario) authentication.getPrincipal();
+		
+		Date dataAtual = new Date();
+		Date dataExpiracao = new Date(dataAtual.getTime() + Long.parseLong(expiration));
+		
+		return Jwts.builder()
+				.setIssuer("API do Fórum da Alura")
+				.setSubject(usuarioLogado.getId().toString())
+				.setIssuedAt(dataAtual)
+				.setExpiration(dataExpiracao)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
+	}
+
+	public boolean isTokenValido(String token) {
+		try {
+			Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+			return true;
+		}catch (Exception e) {
+			
+			return false;
+		}
+	}
+
+	public Long getIdUsuario(String token) {
+		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+		return Long.parseLong(claims.getSubject());
+	}
+
+}
---
+++ src/main/java/br/com/alura/forum/filter/AutenticacaoViaTokenFIlter.java
@@ -0,0 +1,63 @@
+package br.com.alura.forum.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
+
+//neste tipo de classe não da pra injetar dependencias
+public class AutenticacaoViaTokenFIlter extends OncePerRequestFilter{
+
+	private TokenService tokenService;
+	private UsuarioRepository usuarioRepository;
+	
+	public AutenticacaoViaTokenFIlter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = recuperarToken(request);
+		boolean tokenValido = tokenService.isTokenValido(token);
+
+		if(tokenValido) {
+			autenticarCliente(token);
+		}
+		
+		filterChain.doFilter(request, response);
+		
+	}
+
+	private void autenticarCliente(String token) {
+		
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = usuarioRepository.findById(idUsuario).get();
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		
+		String token = request.getHeader("Authorization");
+		
+		if(token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+			return null;
+		}
+		
+		return token.substring(7, token.length());
+	}
+}
